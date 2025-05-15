f254a2176558f7706a4567e36671c02326423d59
Pravalika8864 <lakkampravalika8864@gmail.com>
2025-05-15 20:51:32 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // React dev server
+    methods: ["GET", "POST"]
+  }
+});
+
+mongoose.connect("mongodb://localhost:27017/collabDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const Document = require("./models/Document");
+
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
+
+const findOrCreateDocument = async (id) => {
+  if (!id) return;
+
+  const doc = await Document.findById(id);
+  if (doc) return doc;
+  return await Document.create({ _id: id, data: "" });
+};
+
+server.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
